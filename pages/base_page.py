658e53582398d0a38818dbe16f8d62d4a895ef1b
Alex <alex@Alexs-MBP16.local>
2025-01-05 20:56:55 -0800

    cleanup

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,10 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-# from selenium import webdriver
 
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
-        # self.driver = webdriver.Firefox()
         self._wait = WebDriverWait(self.driver, 10)
 
     def wait_for_element(self, locator):
